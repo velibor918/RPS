bba7ee88b94ba491e982e6b313ad7689116f6859
velibor918 <velibor918@gmail.com>
2023-11-14 14:00:38 +0100

    Added computerChoice function

--- r-p-s.js
+++ r-p-s.js
@@ -1,5 +1,9 @@
-let rules = [`Rock`, `Paper,` `Scissors`];
+let rules = [`Rock`, `Paper` ,`Scissors`];
 
 let playerWinCount = 0;
 let computerWinCount = 0;
+
+function getComputerChoice() {
+    return rules[Math.floor(Math.random() * rules.length)];
+}
 
