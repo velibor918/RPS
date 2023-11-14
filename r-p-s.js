70a5d58235ebdc7b349867632445bd22ffd1e530
velibor918 <velibor918@gmail.com>
2023-11-14 14:22:58 +0100

    Added function that plays one round of RPS

--- r-p-s.js
+++ r-p-s.js
@@ -17,10 +17,32 @@ function getPlayerChoice() {
 
 function playerWin(getPlayerChoice, getComputerChoice) {
     playerWinCount += 1; 
-    console.log(`You win! ${getPlayerChoice} beats ${getComputerChoice}`);
+    console.log(`You win! ${getPlayerChoice} beats ${getComputerChoice}. You have ${playerWinCount} and I have ${computerWinCount}.`);
 }
 
 function computerWin(getPlayerChoice, getComputerChoice) {
     computerWinCount += 1;
-    console.log(`You lose! ${getComputerChoice} beats ${getPlayerChoice}`);
+    console.log(`You lose! ${getComputerChoice} beats ${getPlayerChoice}. You have ${playerWinCount} and I have ${computerWinCount}.`);
+}
+
+function playRPS(getPlayerChoice, getComputerChoice) {
+    if (getComputerChoice === `Rock` && getPlayerChoice === `Rock`) {
+        console.log(`It's a tie!`);
+    } else if (getComputerChoice === `Rock` && getPlayerChoice === `Scissors`) {
+        computerWin(getPlayerChoice, getComputerChoice);
+    } else if (getComputerChoice === `Rock` && getPlayerChoice === `Paper`) {
+        playerWin(getPlayerChoice, getComputerChoice);
+    } else if (getComputerChoice ===`Paper` && getPlayerChoice === `Paper`) {
+        console.log(`It's a tie!`);
+    } else if (getComputerChoice === `Paper` && getPlayerChoice === `Scissors`) {
+        playerWin(getPlayerChoice, getComputerChoice);
+    } else if (getComputerChoice === `Paper` && getPlayerChoice === `Rock`) {
+        computerWin(getPlayerChoice, getComputerChoice)
+    } else if (getComputerChoice === `Scissors` && getPlayerChoice === `Scissors`) {
+        console.log(`It's a tie!`);
+    } else if ( getComputerChoice === `Scissors` && getPlayerChoice ===`Rock`) {
+        playerWin(getPlayerChoice, getComputerChoice);
+    } else if (getComputerChoice === `Scissors` && getPlayerChoice ===`Paper`) {
+        computerWin(getPlayerChoice, getComputerChoice);
+    }
 }
