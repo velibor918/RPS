7fe31b14d8b37ce880b652fe72a1fac6644cb1ec
velibor918 <velibor918@gmail.com>
2024-03-06 12:43:59 +0100

    Removed old code

--- r-p-s.js
+++ r-p-s.js
@@ -11,58 +11,6 @@ function getComputerChoice() {
     return rules[Math.floor(Math.random() * rules.length)];
 }
 
-function getPlayerChoice() {
-    let pChoice = prompt(`Lets play a game of Rock, Paper, Scissors! First to five wins!`); 
-
-    let pChoiceString = pChoice.toString();
-
-    return pChoiceString.charAt(0).toUpperCase() + pChoiceString.slice(1).toLowerCase();
-}
-
-function playerWin(getPlayerChoice, getComputerChoice) {
-    playerWinCount += 1; 
-    console.log(`You win! ${getPlayerChoice} beats ${getComputerChoice}. You have ${playerWinCount} and I have ${computerWinCount}.`);
-}
-
-function computerWin(getPlayerChoice, getComputerChoice) {
-    computerWinCount += 1;
-    console.log(`You lose! ${getComputerChoice} beats ${getPlayerChoice}. You have ${playerWinCount} and I have ${computerWinCount}.`);
-}
-
-/*
-function playRPS(getPlayerChoice, getComputerChoice) {
-    if (getComputerChoice === `Rock` && getPlayerChoice === `Rock`) {
-        console.log(`It's a tie!`);
-    } else if (getComputerChoice === `Rock` && getPlayerChoice === `Scissors`) {
-        computerWin(getPlayerChoice, getComputerChoice);
-    } else if (getComputerChoice === `Rock` && getPlayerChoice === `Paper`) {
-        playerWin(getPlayerChoice, getComputerChoice);
-    } else if (getComputerChoice ===`Paper` && getPlayerChoice === `Paper`) {
-        console.log(`It's a tie!`);
-    } else if (getComputerChoice === `Paper` && getPlayerChoice === `Scissors`) {
-        playerWin(getPlayerChoice, getComputerChoice);
-    } else if (getComputerChoice === `Paper` && getPlayerChoice === `Rock`) {
-        computerWin(getPlayerChoice, getComputerChoice)
-    } else if (getComputerChoice === `Scissors` && getPlayerChoice === `Scissors`) {
-        console.log(`It's a tie!`);
-    } else if ( getComputerChoice === `Scissors` && getPlayerChoice ===`Rock`) {
-        playerWin(getPlayerChoice, getComputerChoice);
-    } else if (getComputerChoice === `Scissors` && getPlayerChoice ===`Paper`) {
-        computerWin(getPlayerChoice, getComputerChoice);
-    }
-}
-*/
-
-function playRPSFive() {
-    playRPS(getPlayerChoice(), getComputerChoice());
-    playRPS(getPlayerChoice(), getComputerChoice());
-    playRPS(getPlayerChoice(), getComputerChoice());
-    playRPS(getPlayerChoice(), getComputerChoice());
-    playRPS(getPlayerChoice(), getComputerChoice());
-}
-
-
-
 function scoreCount(a, b) {
     if (playerWinCount >= 5 && computerWinCount < 5) {
         computerWinCount = 0; 
@@ -139,8 +87,4 @@ scissorsBtn.addEventListener('click', () => {
     }
 
 } );
-
-// playRPSFive();
-
-// scoreCount(playerWinCount, computerWinCount); 
 
